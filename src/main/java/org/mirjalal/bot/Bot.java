fe54193246c551c0cf092b9c9c6a4c86300f4d1b
mir-jalal <mircelal99@gmail.com>
2023-10-08 23:06:28 +0300

    Fixed deprecated Bot class

--- src/main/java/org/mirjalal/bot/Bot.java
+++ src/main/java/org/mirjalal/bot/Bot.java
@@ -7,13 +7,16 @@ public class Bot extends TelegramLongPollingBot {
     public static String BOT_USERNAME = "BOT_USERNAME";
     public static String BOT_TOKEN = "BOT_TOKEN";
 
+    public Bot() {
+        super(getToken());
+    }
+
     @Override
     public String getBotUsername() {
         return System.getenv(BOT_USERNAME);
     }
 
-    @Override
-    public String getBotToken() {
+    public static String getToken() {
         return System.getenv(BOT_TOKEN);
     }
 
